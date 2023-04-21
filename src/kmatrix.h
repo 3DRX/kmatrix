f1268c780aff069c5f940ec52f3e2827bc4a391d
3DRX <kjy20030228@icloud.com>
2023-04-21 21:47:52 +0800

    move project into single .h .c

--- src/kmatrix.h
+++ src/kmatrix.h
@@ -1,7 +1,21 @@
 #ifndef KMATRIX
 #define KMATRIX
 
-#include "datastructs.h"
+#include <stddef.h>
+#include <stdbool.h>
+
+#define KM_DATA long double
+
+typedef struct {
+	size_t dim;
+	KM_DATA **value;
+} KMatrix;
+
+typedef struct {
+	size_t shape0;
+	size_t shape1;
+	KM_DATA *value;
+} KArray;
 
 KMatrix *KMat_create(const size_t dim, KM_DATA **value);
 KMatrix *KMat_copy(const KMatrix *m);
@@ -15,5 +29,28 @@ KMatrix *KMat_dot(const KMatrix *x, const KMatrix *y);
 KMatrix *KMat_inverse(const KMatrix *m);
 KM_DATA KMat_determinant(const KMatrix *m);
 KArray *KMat_toKArr(const KMatrix *m);
+
+KArray *KArr_create(const size_t shape0, const size_t shape1, KM_DATA *value);
+KArray *KArr_copy(const KArray *a);
+KArray *KArr_zeros(const size_t shape0, const size_t shape1);
+KArray *KArr_ones(const size_t shape0, const size_t shape1);
+bool KArr_set(const KArray *a, const size_t col, const size_t row,
+	      KM_DATA value);
+KM_DATA KArr_get(const KArray *a, const size_t col, const size_t row);
+void KArr_delete(KArray *a);
+KArray *KArr_T(const KArray *a);
+void KArr_print(const KArray *a);
+KArray *KArr_dot(const KArray *x, const KArray *y);
+KMatrix *KArr_toKMat(const KArray *a);
+KArray *KArr_add(const KArray *a, const KArray *b);
+KArray *KArr_sub(const KArray *a, const KArray *b);
+KArray *KArr_mul(const KArray *a, const KArray *b);
+KArray *KArr_div(const KArray *a, const KArray *b);
+KArray *KArr_addnum(const KArray *a, const KM_DATA b);
+KArray *KArr_subnum(const KArray *a, const KM_DATA b);
+KArray *KArr_mulnum(const KArray *a, const KM_DATA b);
+KArray *KArr_divnum(const KArray *a, const KM_DATA b);
+bool KArr_equal(const KArray *a, const KArray *b);
+KArray *KArr_reshape(const KArray *a, const size_t shape0, const size_t shape1);
 
 #endif
